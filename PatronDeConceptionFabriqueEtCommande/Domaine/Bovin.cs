94efb63a8b551503d3d5246febc8eb3fac532456
PatrickBulle <patrick.bulle@synergie-est.fr>
2020-02-22 09:55:12 +0100

    Création de l'interface

---
+++ PatronDeConceptionFabriqueEtCommande/Domaine/Bovin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronDeConceptionFabriqueEtCommande.Domaine
+{
+    class Bovin
+    {
+        public String Copaip { get; set; }
+        public String Nunati { get; set; }
+        public String Nobovi { get; set; }
+    }
+}
